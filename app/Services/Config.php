c0f05428c7dff6aebfa39a590f0f1af763c153dd
SakuraSa233 <sakura.sa233@gmail.com>
2018-07-04 11:16:28 +0800

    add more auth_chain obfs plugin

--- app/Services/Config.php
+++ app/Services/Config.php
@@ -84,17 +84,17 @@ class Config
                 return $list;
             case 'protocol':
                 $list = array('origin', 'verify_deflate',
-                            'auth_sha1_v4', 'auth_sha1_v4_compatible', 'auth_aes128_sha1', 'auth_aes128_md5', 'auth_chain_a', 'auth_chain_b');
+                            'auth_sha1_v4', 'auth_sha1_v4_compatible', 'auth_aes128_sha1', 'auth_aes128_md5', 'auth_chain_a', 'auth_chain_b', 'auth_chain_c', 'auth_chain_d', 'auth_chain_e', 'auth_chain_f');
                 return $list;
             case 'allow_none_protocol':
-                $list = array('auth_chain_a', 'auth_chain_b');
+                $list = array('auth_chain_a', 'auth_chain_b', 'auth_chain_c', 'auth_chain_d', 'auth_chain_e', 'auth_chain_f');
                 return $list;
             case 'relay_able_protocol':
-                $list = array('auth_aes128_md5', 'auth_aes128_sha1', 'auth_chain_a', 'auth_chain_b');
+                $list = array('auth_aes128_md5', 'auth_aes128_sha1', 'auth_chain_a', 'auth_chain_b', 'auth_chain_c', 'auth_chain_d', 'auth_chain_e', 'auth_chain_f');
                 return $list;
             case 'ss_aead_method':
                 $list = array('aes-128-gcm', 'aes-192-gcm',
-                'aes-256-gcm', 'chacha20-ietf-poly1305', 'xchacha20-ietf-poly1305');
+                'aes-256-gcm', 'chacha20-ietf-poly1305', 'xchacha20-ietf-poly1305', 'sodium:aes-256-gcm');
                 return $list;
             case 'ss_obfs':
                 $list = array('simple_obfs_http', 'simple_obfs_http_compatible', 'simple_obfs_tls', 'simple_obfs_tls_compatible');
@@ -104,7 +104,7 @@ class Config
                             'aes-128-ctr', 'aes-192-ctr', 'aes-256-ctr', 'camellia-128-cfb', 'camellia-192-cfb', 'camellia-256-cfb',
                             'bf-cfb', 'cast5-cfb', 'des-cfb', 'des-ede3-cfb', 'idea-cfb',
                             'rc2-cfb', 'seed-cfb', 'salsa20', 'chacha20', 'chacha20-ietf', 'none', 'aes-128-gcm', 'aes-192-gcm',
-                            'aes-256-gcm', 'chacha20-ietf-poly1305', 'xchacha20-ietf-poly1305');
+                            'aes-256-gcm', 'chacha20-ietf-poly1305', 'xchacha20-ietf-poly1305', 'sodium:aes-256-gcm');
                 return $list;
         }
     }
