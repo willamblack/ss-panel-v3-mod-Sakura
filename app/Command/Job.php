0224a93b9c5a0dd8e774d5488c20239658676ed7
SakuraSa233 <sakura.sa233@gmail.com>
2018-08-21 21:12:35 +0800

    fix: switch dns to origin node

--- app/Command/Job.php
+++ app/Command/Job.php
@@ -558,7 +558,7 @@ class Job
                         $query->where('node_group', '=', $node->node_group)
                             ->orWhere('node_group', '=', 0);
                         }
-                    )->whereRaw('UNIX_TIMESTAMP()-`node_heartbeat`<300')->inRandomOrder()->first();
+                    )->where('online_status', 1)->inRandomOrder()->first();
 
                     switch(Config::get('node_switcher'))
                     {
