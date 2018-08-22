1165c93486f48bc8e00837766ee62e25e0a45985
SakuraSa233 <sakura.sa233@gmail.com>
2018-08-22 17:05:50 +0800

    fix: lost ">"

--- app/Command/Job.php
+++ app/Command/Job.php
@@ -558,7 +558,7 @@ class Job
                         $query->where('node_group', '=', $node->node_group)
                             ->orWhere('node_group', '=', 0);
                         }
-                    )-whereRaw('UNIX_TIMESTAMP()-`node_heartbeat` < 60')->inRandomOrder()->first();
+                    )->whereRaw('UNIX_TIMESTAMP() - `node_heartbeat` < 60')->inRandomOrder()->first();
 
                     switch(Config::get('node_switcher'))
                     {
